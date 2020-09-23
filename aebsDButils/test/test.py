8ef49ad183a545af456ff3fc959afe5098bdce8d
Ólavur Mortensen <olavurmortensen@gmail.com>
2020-09-23 10:59:05 +0100

    Added basic test.

--- aebsDButils/test/test.py
+++ aebsDButils/test/test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import unittest, logging
+from aebsDButils.ged2csv import Ged2Genealogy
 
 logging.basicConfig(level=logging.INFO)
 
@@ -8,8 +9,10 @@ logging.basicConfig(level=logging.INFO)
 TEST_DATA_DIR = 'aebsDButils/test/test_data/'
 
 TEST_GED = TEST_DATA_DIR + 'small_test_tree.ged'
-TEST_CSV = TEST_DATA_DIR +'correct_results.csv'
+TEST_CSV = TEST_DATA_DIR +'small_test_tree.csv'
+TEST_CSV_CORRECT = TEST_DATA_DIR +'correct_results.csv'
 TEST_IND = TEST_DATA_DIR +'test_individuals.txt'
+
 
 class Tests(unittest.TestCase):
 
@@ -17,9 +20,10 @@ class Tests(unittest.TestCase):
         logging.info('Set up tests')
         logging.info('------------')
 
-    def test1(self):
-        logging.info('Test 1')
+    def write_gen_csv(self):
+        logging.info('Write AEBS genealogy to CSV')
         logging.info('------------')
+        Ged2Genealogy(TEST_GED, TEST_CSV)
 
     def tearDown(self):
         logging.info('------------')
