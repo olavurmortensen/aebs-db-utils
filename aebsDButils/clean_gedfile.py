e97b52288bfbd6c14b64b3dc23365ab9b338b7c4
Ólavur Mortensen <olavurmortensen@gmail.com>
2020-09-25 10:48:41 +0100

    dos2unix and remove_unwanted_newlines now take text as input and output text, rather than reading and writing files. The clean_gedfile function reads the GED file, cleans it, and writes it again. This is still completely untested.

--- aebsDButils/clean_gedfile.py
+++ aebsDButils/clean_gedfile.py
@@ -8,35 +8,46 @@ from aebsDButils.utils import is_int
 
 logging.basicConfig(level=logging.INFO)
 
-def dos2unix(inpath, outpath):
+def dos2unix(intext, outtext):
     '''
     Convert DOS linefeeds (crlf) to unix (lf).
 
-    This code was copied from the followingproject:
+    This code was adapted from the followingproject:
     https://github.com/techtonik/dos2unix
 
     Arguments:
     ----------
     inpath  :   String
-        Path to input file.
+        Input file as a string.
     outpath :   String
-        Path to output file.
+        Output file as a string.
     '''
 
-    content = ''
     outsize = 0
-    with open(inpath, 'rb') as infile:
-        content = infile.read()
-    with open(outpath, 'wb') as output:
-        for line in content.splitlines():
-	    outsize += len(line) + 1
-	    output.write(line + b'\n')
+    outtext = ''
+    for line in intext.splitlines():
+        outsize += len(line) + 1
+        outtext += line + b'\n'
 
-    logging.info("Done. Stripped %s bytes." % (len(content) - outsize))
+    logging.info("Done. Stripped %s bytes." % (len(intext) - outsize))
 
-def remove_unwanted_newlines(inpath, outpath):
-    with open(inpath) as fid:
-        inlines = fid.readlines()
+    return outtext
+
+
+def remove_unwanted_newlines(intext, outtext):
+    '''
+    The GED file often has some newlines that causes ged4py to be unable to parse
+    the file. We remove these lines in this function.
+
+    Arguments:
+    ----------
+    inpath  :   String
+        Input file as a string.
+    outpath :   String
+        Output file as a string.
+    '''
+
+    inlines = intext.splitlines()
 
     n_empty_lines = 0
     outtext = ''
@@ -46,7 +57,7 @@ def remove_unwanted_newlines(inpath, outpath):
 
         # Just print first line, which should be '0 HEAD'.
         if i == 0:
-            outtext.append(line)
+            outtext += line
             continue
 
         if len(line) == 0:
@@ -55,15 +66,32 @@ def remove_unwanted_newlines(inpath, outpath):
             continue
         elif not is_int(line[0]):
             # Continuation of previous line.
-            outtext.append(' ' + line)
+            outtext += ' ' + line
         else:
             # End previous line with newline.
             # Write the next line.
-            print('\n' + line, end='')
-            outtext.append('\n' + line)
+            outtext += '\n' + line
 
     logging.info('Discarded %d empty lines' % n_empty_lines)
 
+    return outtext
+
+def clean_gedfile(inpath, outpath):
+    '''
+    Arguments:
+    ----------
+    inpath  :   String
+        Path to input file.
+    outpath :   String
+        Path to output file.
+    '''
+
+    with open(inpath) as fid:
+        text = fid.read()
+
+    text_unix = dos2unix(text)
+    text_stripped = remove_unwanted_newlines(text_unix)
+
     with open(outpath, 'w') as fid:
-        fid.write(outtext)
+        fid.write(text_stripped)
 
